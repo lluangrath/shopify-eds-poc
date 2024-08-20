4b5e94655c2d0336b7330222eed49d1067a1c44a
Larry Luangrath <lluangrath@live.com>
2024-08-20 00:13:03 -0500

    add RouteComponent

---
+++ blocks/route/route.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line import/no-unresolved,import/extensions
+import { h, Component, Fragment, render } from '../../scripts/preact.js';
+// eslint-disable-next-line import/no-unresolved,import/extensions
+import htm from '../../scripts/htm.js';
+
+const html = htm.bind(h);
+
+function App() {
+  return html`
+    <div>
+      <h1>Preact header component</h1>
+      <p>This is a description text</p>
+    </div>
+  `;
+}
+
+class RouteComponent extends Component {
+render() {
+    return html`
+      <${Fragment}>
+        <div class="route-comp">
+          <h3>This is a Route Component</h3>
+        </div>
+      <//>
+    `;
+  }
+}
+
+export default function decorate(block) {
+    const rtApp = html`<${App}/>`;
+    const rt = html`<${RouteComponent} />`;
+  }
