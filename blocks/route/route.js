b6e944281a9f5756b453c6c210d249e972b0352c
Larry Luangrath <lluangrath@live.com>
2024-08-20 08:55:39 -0500

    add routing components

--- blocks/route/route.js
+++ blocks/route/route.js
@@ -2,33 +2,49 @@
 import { h, Component, Fragment, render } from '../../scripts/preact.js';
 // eslint-disable-next-line import/no-unresolved,import/extensions
 import htm from '../../scripts/htm.js';
+// eslint-disable-next-line import/no-unresolved,import/extensions
+import { LocationProvider, Router, Route } from '../../scripts/router.js';
 
 const html = htm.bind(h);
 
 function App() {
   return html`
-    <div>
-      <h1>Preact header component</h1>
-      <p>This is a description text</p>
-    </div>
+    <${LocationProvider}>
+        <${Router}>
+            <${Products} path="/products" />
+            <${Route} path="product" component=${ProductHome} />
+        <//>
+    <//>
   `;
 }
 
-class RouteComponent extends Component {
+class Products extends Component {
 render() {
     return html`
       <${Fragment}>
         <div class="route-comp">
-          <h3>This is a Route Component</h3>
+          <h3>This is a Route Component for Products Page</h3>
         </div>
       <//>
     `;
   }
 }
 
+class ProductHome extends Component {
+    render() {
+        return html`
+          <${Fragment}>
+            <div class="route-comp">
+              <h3>Product Home</h3>
+            </div>
+          <//>
+        `;
+      }
+    }
+
 export default async function decorate(block) {
     const rtApp = html`<${App}/>`;
-    const rt = html`<${RouteComponent} />`;
-
-    render(rt, block);
+    render(rtApp, block);
+    //const rt = html`<${Products} />`;
+    //render(rt, block);
   }
