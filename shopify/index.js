efc3feb17cad2f0e3ea3e5bb8732ecab7d9f22bd
Larry Luangrath <lluangrath@live.com>
2024-08-21 23:30:28 -0500

    remove extra product list import

--- shopify/index.js
+++ shopify/index.js
@@ -9,7 +9,6 @@ export const PUBLIC_STOREFRONT_API_TOKEN = '0f3a32c356f27e7ab24a28674bc60492';
 export const PUBLIC_STORE_DOMAIN = 'slalom-aem.myshopify.com';
 export const GRAPHQL_ENDPOINT = 'https://slalom-aem.myshopify.com/api/2024-07/graphql.json';
 export const html = htm.bind(h);
-import { ProductList } from './product-list/product-list.js';
 
 export class ProductHome extends Component {
 
