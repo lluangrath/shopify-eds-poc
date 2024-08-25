998032133e25f213310f57b764e53903543e2d18
Larry Luangrath <lluangrath@live.com>
2024-08-25 11:20:37 -0500

    add productCategory data

--- blocks/preact-app/preact-app.js
+++ blocks/preact-app/preact-app.js
@@ -5,6 +5,7 @@ export default async function decorate(block) {
   
   const appDiv = document.createElement("div");
   appDiv.id = "app";
+  appDiv.dataset.productCategory = "snowboard";
 
   block.textContent = '';
   block.append(appDiv);
